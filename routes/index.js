5a5136e0a8d5fbae9009f2daceef3058bdf4c098
Michael Jordan <michaeljordan99@hotmail.com>
2013-09-10 16:00:14 -0500

    added demo1

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@
  * GET home page.
  */
 module.exports = function(app) {
+
 	app.get('/', function(req, res) {
 		res.render('index', { title: 'Demo' })
+	});
+
+	app.get('/demo1', function(req, res) {
+		res.render('demo1', { title: 'Demo 1' })
 	});
 };
